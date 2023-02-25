dc6eeeecd7927768a93a97492c0a6d4ab02a66ac
Andrew Oliveira <andrew.oliveira@sinqia.com.br>
2023-02-25 00:08:05 -0300

    Adicionando formulários de cliente

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -7,14 +7,39 @@
         <button class="btn btn-outline-dark" type="submit">Search</button>
       </form>
     </div>
-    
+
     <div class="container d-flex justify-content-end ">
-      <router-link  class="p-2 nav-link active" type="button" to="">
-        <font-awesome-icon icon="fa-solid fa-user" />
-        Andrew
-      </router-link>
-      <router-link  class="nav-link active" type="button" to="/pedidos">
+      <div class="dropdown">
+        <router-link class="p-2 nav-link dropdown-toggle" type="button" role="button" to="/" data-bs-toggle="dropdown"
+          aria-expanded="false">
+          <font-awesome-icon icon="fa-solid fa-user" />
+          Andrew
+        </router-link>
+        <ul class="dropdown-menu">
+          <li>
+            <div class="dropdown-item disabled text-reset">Olá Andrew!</div>
+            <div class="dropdown-item disabled form-text">Você possui 1pts</div>
+          </li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><router-link class="dropdown-item" href="#" to="/">Cupons</router-link></li>
+          <li><router-link class="dropdown-item" href="#" to="/cliente/perfil">Meu Perfil</router-link></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><router-link class="dropdown-item" href="#" to="/">Painel de administador</router-link></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><router-link class="dropdown-item" href="#" to="/">Sair</router-link></li>
+        </ul>
+      </div>
+      <router-link class="p-2 nav-link active" type="button" to="/">
         Compras
+      </router-link>
+      <router-link class="p-2 nav-link active" type="button" to="/">
+        <font-awesome-icon icon="fa-solid fa-cart-shopping" />
       </router-link>
     </div>
   </nav>
---
+++ src/views/EditAdressClientView.vue
@@ -0,0 +1,104 @@
+<template>
+	<div class="container">
+		<label for="basic-url" class="form-label fs-3">Editar endereço</label>
+		<div id="divEnderecoEntrega">
+			<div class="row">
+				<div class="col-sm">
+					<label for="basic-url" class="form-label">Identificação <label class="form-text"> (Utilize um nome
+							composto na
+							identificação do endereço)</label></label>
+					<div class="input-group">
+						<input type="email" class="form-control" id="txtEmail" aria-describedby="basic-addon3">
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-2">
+					<label for="basic-url" class="form-label">CEP</label>
+					<div class="input-group">
+						<input type="text" class="form-control" id="txtCep" aria-describedby="basic-addon3">
+					</div>
+				</div>
+				<div class="col-sm-2">
+					<label for="basic-url" class="form-label">Tipo de residencia</label>
+					<div class="input-group">
+						<select class="form-select" id="slctTipoLogradouro">
+							<option selected>Escolha...</option>
+							<option value="1">Casa</option>
+							<option value="2">Apartamento</option>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-2">
+					<label for="basic-url" class="form-label">Tipo de logradouro</label>
+					<div class="input-group">
+						<select class="form-select" id="slctTipoLogradouro">
+							<option selected>Escolha...</option>
+							<option value="1">Rua</option>
+							<option value="2">Avenida</option>
+							<option value="3">Estrada</option>
+							<option value="3">Marginal</option>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<label for="basic-url" class="form-label">Logradouro</label>
+					<div class="input-group">
+						<input type="text" class="form-control" id="txtDtaNascimento" aria-describedby="basic-addon3">
+					</div>
+				</div>
+				<div class="col-sm-2">
+					<label for="basic-url" class="form-label">Numero</label>
+					<div class="input-group">
+						<input type="text" class="form-control" id="txtDtaNascimento" aria-describedby="basic-addon3">
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-3">
+					<label for="basic-url" class="form-label">Pais</label>
+					<div class="input-group">
+						<select class="form-select" id="inputGroupSelect01">
+							<option selected>Escolha...</option>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-3">
+					<label for="basic-url" class="form-label">Estado</label>
+					<div class="input-group">
+						<select class="form-select" id="inputGroupSelect01">
+							<option selected>Escolha...</option>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-3">
+					<label for="basic-url" class="form-label">Cidade</label>
+					<div class="input-group">
+						<select class="form-select" id="inputGroupSelect01">
+							<option selected>Escolha...</option>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-3">
+					<label for="basic-url" class="form-label">Bairro</label>
+					<div class="input-group">
+						<select class="form-select" id="inputGroupSelect01">
+							<option selected>Escolha...</option>
+						</select>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row d-flex justify-content-between p-3">
+			<router-link to="/" type="button" class="btn btn-outline-warning">Salvar</router-link>
+		</div>
+	</div>
+</template>
+ 
+<script>
+export default {
+	name: "EditAdressClientView"
+}
+</script>
+ 
+<style></style>
