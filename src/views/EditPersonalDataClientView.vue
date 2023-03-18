707eb131101b4bcda52782e5bb42671289a95759
Thiago Araujo <80802325@redecorp.br>
2023-03-18 17:18:53 -0300

    feat: add style and card on view profile data client

--- src/views/EditPersonalDataClientView.vue
+++ src/views/EditPersonalDataClientView.vue
@@ -1,67 +1,90 @@
 <template>
     <div class="container">
-        <label class="form-label fs-3">Alterar dados pessoais</label>
-        <div class="row">
-            <div class="col-sm-4">
-                <label for="basic-url" class="form-label">CPF</label>
+        <div id="divNome" class="card mt-3 p-2 cardForm p-4">
+            <label class="form-label fs-3">Alterar dados pessoais</label>
+            <div class="row mt-3">
+                <div class="col-sm-6">
+                <label for="basic-url" class="form-label">Nome</label>
                 <div class="input-group">
-                    <input type="text" class="form-control" id="cpf" name="cpf" v-model="client.cpf" aria-describedby="basic-addon3">
+                    <input type="text" class="form-control" id="name" name="name" v-model="client.name" aria-describedby="basic-addon3">
+                </div>
+                </div>
+                <div class="col-sm-6">
+                <label for="basic-url" class="form-label">Sobrenome</label>
+                <div class="input-group">
+                    <input type="text" class="form-control" id="lastName" name="lastName" v-model="client.lastName" aria-describedby="basic-addon3">
+                </div>
                 </div>
             </div>
-            <div class="col-sm-4">
-                <label for="basic-url" class="form-label">Data de nascimento</label>
-                <div class="input-group">
-                    <input type="date" class="form-control" id="dateOfBirth" name="dateOfBirth" v-model="client.dateOfBirth" aria-describedby="basic-addon3">
+
+            <div class="row">
+                <div class="col-sm-4">
+                    <label for="basic-url" class="form-label">CPF</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="cpf" name="cpf" v-model="client.cpf" aria-describedby="basic-addon3">
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <label for="basic-url" class="form-label">Data de nascimento</label>
+                    <div class="input-group">
+                        <input type="date" class="form-control" id="dateOfBirth" name="dateOfBirth" v-model="client.dateOfBirth" aria-describedby="basic-addon3">
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <label for="basic-url" class="form-label">Genero</label>
+                    <div class="input-group">
+                        <select class="form-select" id="gender" name="gender" v-model="client.gender">
+                            <option disabled value="">Escolha...</option>
+                            <option v-for="option in options.genders" :value="option">
+                                {{ option }}
+                            </option>
+                        </select>
+                    </div>
                 </div>
             </div>
-            <div class="col-sm-4">
-                <label for="basic-url" class="form-label">Genero</label>
-                <div class="input-group">
-                    <select class="form-select" id="gender" name="gender" v-model="client.gender">
-                        <option disabled value="">Escolha...</option>
-                        <option v-for="option in options.genders" :value="option">
-                            {{ option }}
-                        </option>
-                    </select>
-                </div>
-            </div>
-        </div>
-        
-        <div class="row mt-3">
-            <label for="basic-url" class="form-label">E-mail</label>
-            <div class="col-sm">
-                <div class="input-group">
-                    <input type="email" class="form-control" id="email" name="email" v-model="client.email" aria-describedby="basic-addon3">
-                </div>
-            </div>    
         </div>
 
-        <div class="row  mt-3" id="divContato">
-            <label for="basic-url" class="form-label">Contato</label>
-            <div class="col-sm-4">
-                <label for="basic-url" class="form-label">Tipo de telefone</label>
-                <div class="input-group">
-                <select class="form-select" id="typePhone" name="typePhone" v-model="client.typePhone">
-                <option disabled value="">Escolha...</option>
-                    <option v-for="option in options.typesPhone" :value="option">
-                    {{ option }}
-                    </option>
-                </select>
-                </div>
+        <div id="divNome" class="card mt-3 p-2 cardForm p-4">
+            <label class="form-label fs-3">Alterar acesso e contato</label>
+            <div class="row mt-3">
+                <label for="basic-url" class="form-label">E-mail</label>
+                <div class="col-sm">
+                    <div class="input-group">
+                        <input type="email" class="form-control" id="email" name="email" v-model="client.email" aria-describedby="basic-addon3">
+                    </div>
+                </div>    
             </div>
-            <div class="col-sm-2">
-                <label for="basic-url" class="form-label">DDD</label>
-                <div class="input-group">
-                    <input type="text" class="form-control" id="dddLocation" name="dddLocation" v-model="client.dddLocation" aria-describedby="basic-addon3">
+
+            <div class="row  mt-3" id="divContato">
+                <label for="basic-url" class="form-label">Contato</label>
+                <div class="col-sm-4">
+                    <label for="basic-url" class="form-label">Tipo de telefone</label>
+                    <div class="input-group">
+                    <select class="form-select" id="typePhone" name="typePhone" v-model="client.typePhone">
+                    <option disabled value="">Escolha...</option>
+                        <option v-for="option in options.typesPhone" :value="option">
+                        {{ option }}
+                        </option>
+                    </select>
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm-4">
-                <label for="basic-url" class="form-label">Numero</label>
-                <div class="input-group">
-                    <input type="text" class="form-control" id="phoneNumber" name="phoneNumber" v-model="client.phoneNumber" aria-describedby="basic-addon3">
+                <div class="col-sm-2">
+                    <label for="basic-url" class="form-label">DDD</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="dddLocation" name="dddLocation" v-model="client.dddLocation" aria-describedby="basic-addon3">
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <label for="basic-url" class="form-label">Numero</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="phoneNumber" name="phoneNumber" v-model="client.phoneNumber" aria-describedby="basic-addon3">
+                    </div>
                 </div>
             </div>
         </div>
+
+        
+        
 
     </div>
 </template>
@@ -79,8 +102,8 @@ export default {
             dateOfBirth: '05/02/2000',
             gender: 'Masculino',
             email: 'andrew.santos@fatec.sp.gov.br',
-            // name: '',
-            // lastName: '',
+            name: 'Andrew',
+            lastName: 'Oliveira',
             phoneNumber: '984661480',
             dddLocation: '11',
             typePhone: 'Celular',
